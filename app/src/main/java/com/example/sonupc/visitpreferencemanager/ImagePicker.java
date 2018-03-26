176d2c5301bdaa5fa8e41a685de3ab0635030f34
sonupc <abh.saxena10@gmail.com>
2018-03-27 05:10:45 +0530

    1) Added picasso for image loading.

--- app/src/main/java/com/example/sonupc/visitpreferencemanager/ImagePicker.java
+++ app/src/main/java/com/example/sonupc/visitpreferencemanager/ImagePicker.java
@@ -1,8 +1,12 @@
 package com.example.sonupc.visitpreferencemanager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +21,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class ImagePicker extends AppCompatActivity implements View.OnClickListener{
@@ -72,8 +81,34 @@ public class ImagePicker extends AppCompatActivity implements View.OnClickListen
             case 0:
                 if(resultCode == RESULT_OK){
                     selectedImage = imageReturnedIntent.getData();
-                    Log.d(TAG, "Got uri: " + selectedImage);
-                    imageView.setImageURI(selectedImage);
+
+                    File file = new File(selectedImage.getPath());
+                    long length = file.length();
+                    length = length/1024;
+                    System.out.println("File Path : " + file.getPath() + ", File size : " + length +" KB");
+
+                    /*String absPath = file.getAbsolutePath();
+                    Log.d(TAG, "absPath: " + absPath);
+                    Log.d(TAG, "path: " + selectedImage.getPath());
+                    //Bitmap resized = ThumbnailUtils.extractThumbnail(BitmapFactory.decodeFile(selectedImage.getPath(), width, height);
+                    BitmapFactory.Options mBitmapOptions = new BitmapFactory.Options();
+                    mBitmapOptions.inSampleSize = 4;
+
+                    Bitmap img = BitmapFactory.decodeFile(selectedImage.getPath(), mBitmapOptions);
+                    imageView.setImageBitmap(img);
+                    Log.d(TAG, "Got uri: " + selectedImage);*/
+
+
+                    Picasso.get().load(selectedImage).into(imageView);
+
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inJustDecodeBounds = true;
+                    BitmapFactory.decodeFile(selectedImage.getPath(), options);
+                    int imageHeight = options.outHeight;
+                    int imageWidth = options.outWidth;
+                    String imageType = options.outMimeType;
+                    Log.d(TAG, "height: " + imageHeight + ", width: " + imageWidth + ", type: " + imageType);
+
                 }
                 else{
                     Log.e(TAG, " Error picking image");
@@ -103,4 +138,39 @@ public class ImagePicker extends AppCompatActivity implements View.OnClickListen
         });
 
     }
+
+
+    /*public Bitmap getThumbnail(Uri uri) throws FileNotFoundException, IOException {
+        InputStream input = this.getContentResolver().openInputStream(uri);
+
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither=true;//optional
+        onlyBoundsOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
+
+        if ((onlyBoundsOptions.outWidth == -1) || (onlyBoundsOptions.outHeight == -1)) {
+            return null;
+        }
+
+        int originalSize = (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth) ? onlyBoundsOptions.outHeight : onlyBoundsOptions.outWidth;
+
+        double ratio = (originalSize > THUMBNAIL_SIZE) ? (originalSize / THUMBNAIL_SIZE) : 1.0;
+
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio);
+        bitmapOptions.inDither = true; //optional
+        bitmapOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//
+        input = this.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+        return bitmap;
+    }
+
+    private static int getPowerOfTwoForSampleRatio(double ratio){
+        int k = Integer.highestOneBit((int)Math.floor(ratio));
+        if(k==0) return 1;
+        else return k;
+    }*/
 }
